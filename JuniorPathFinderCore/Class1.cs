5dd1592384a557d935205a53db2839935e9d62fe
AdminPC <AdminPC@ADMINPC_>
2019-06-04 21:28:11 +0700

    Добавьте файлы проекта.

---
+++ JuniorPathFinderCore/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuniorPathFinderCore
+{
+    public class Class1
+    {
+    }
+}
