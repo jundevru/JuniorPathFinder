e03e292cd7427b0648f583c4ef3e796e534d265f
JunDevRu <jundevru@gmail.com>
2019-06-04 22:30:06 +0700

    Add elements: RegionItem, TreeQueue and Layers

---
+++ JuniorPathFinderCore/Items/RegionItem.cs
@@ -0,0 +1,60 @@
+﻿//region element
+using System;
+
+
+namespace JuniorPathFinderCore
+{
+    [Serializable]
+    class RegionItem
+    {
+        /// <summary>
+        /// X local horizontal coordinate
+        /// </summary>
+        public byte xLocalCoordinate;
+        /// <summary>
+        /// Y or Z local horizontal coordinate
+        /// </summary>
+        public byte xxLocalCoordinate;
+        /// <summary>
+        /// Y or Z local vertical coordinate
+        /// </summary>
+        public int hLocalCoordinate;
+        /// <summary>
+        /// Layer
+        /// </summary>
+        public Layers layer;
+
+        [NonSerialized]
+        public RegionItem parent = null;
+        [NonSerialized]
+        public byte opened = 0;
+        [NonSerialized]
+        public byte closed = 0;
+        [NonSerialized]
+        public int g = 0;
+        [NonSerialized]
+        public double f = 0;
+
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        /// <param name="x">X horizontal coordinate</param>
+        /// <param name="xx">Y or Z horizontal coordinate</param>
+        /// <param name="h">Y or Z vertical coordinate)</param>
+        /// <param name="layer">Layer number</param>
+        public RegionItem(byte x, byte xx, int h, Layers layer)
+        {
+            xLocalCoordinate = x;
+            xxLocalCoordinate = xx;
+            hLocalCoordinate = h;
+            this.layer = layer;
+        }
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        public RegionItem()
+            :this(0,0,0, Layers.Layer0)
+        { }
+
+    }
+}
---
+++ JuniorPathFinderCore/Layers/LayerMask.cs
@@ -0,0 +1,63 @@
+﻿// Layer mask
+namespace JuniorPathFinderCore
+{
+    public sealed class LayerMask
+    {
+        public static readonly LayerMask Empty = new LayerMask();
+
+        int value = 0;
+
+        public LayerMask() { value = 0; }
+
+        /// <summary>
+        /// Get value
+        /// </summary>
+        public int Value { get { return value; } }
+
+        /// <summary>
+        /// Check the entry of a layer in the mask
+        /// </summary>
+        /// <param name="layer"></param>
+        /// <returns></returns>
+        public bool Contains(Layers layer)
+        {
+            return !((value & (1 << (int)layer)) == 0);
+        }
+
+        /// <summary>
+        /// Add layer to mask
+        /// </summary>
+        /// <param name="layer"></param>
+        /// <returns></returns>
+        public bool Add(Layers layer)
+        {
+            if (Contains(layer)) return false;
+            value = value | (1 << (int)layer);
+            return true;
+        }
+
+        /// <summary>
+        /// Remove Layer from mask
+        /// </summary>
+        /// <param name="layer"></param>
+        /// <returns></returns>
+        public bool Remove(Layers layer)
+        {
+            if (!Contains(layer)) return false;
+            value = value ^ (int)layer;
+            return true;
+        }
+
+        /*
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public bool Equals(LayerMask other)
+        {
+            return (this.value == other.Value);
+        }
+        */
+    }
+}
